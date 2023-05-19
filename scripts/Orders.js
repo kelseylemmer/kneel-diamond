2dc929fd2ba8cde19cff8eac4a1ce5b960d4101c
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-05-19 11:40:49 -0400

    made changes to display total order price using price of all selected options

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,12 +1,13 @@
 export const Orders = async () => {
-    const fetchResponse = await fetch("http://localhost:8088/orders")
+    const fetchResponse = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await fetchResponse.json()
 
     let ordersHTML = ""
     const ordersArray = orders.map(
         (order) => {
+            const orderPrice = order.metal.price + order.style.price + order.size.price
             return `<section class="orders">
-                <div>Order ${order.id}</div>
+                <div>Order #${order.id} cost $${orderPrice}</div>
             
         </section>`
 
